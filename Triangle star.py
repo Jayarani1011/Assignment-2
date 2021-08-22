189ebe5686a5cca089d8b5522fec2a7d20652c09
Jayarani1011 <83907258+Jayarani1011@users.noreply.github.com>
2021-08-22 11:36:48 +0530

    Create Triangle star.py

---
+++ Triangle star.py
@@ -0,0 +1,24 @@
+print("print equilateral triangle pyramid using asterisk symbol")
+
+#printing full triangle pyramid using stars
+
+size = 5
+
+m=(2* size)-2
+
+for i in range(0,size):
+
+    for j in range(0,m):
+
+        print(end=" ")
+
+    #decrementing m after each loop
+
+    m=m-1
+
+    for j in range(0,i+1):
+
+        print("*", end=' ')
+
+    print( " " )
+
